c9a8d926618dd9cbb105b618b94bca58a4970354
Bartek <bartek.planiak@gmail.com>
2020-12-13 00:24:13 +0100

    Initiate android views in MainActivity

--- app/src/main/java/com/example/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.mylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWishlist, btnCurrentlyReading, btnFavourite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
+
+    private void initViews(){
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWishlist = findViewById(R.id.btnWishlist);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnFavourite = findViewById(R.id.btnFavourite);
+        btnAbout = findViewById(R.id.btnAbout);
+    }
+
 }
