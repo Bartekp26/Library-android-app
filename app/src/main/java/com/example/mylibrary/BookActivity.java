83f43deea9a08c6684706498ac0203bd0af384c5
Bartek <bartek.planiak@gmail.com>
2020-12-30 19:07:03 +0100

    Add layout to book activity & initialise views

--- app/src/main/java/com/example/mylibrary/BookActivity.java
+++ app/src/main/java/com/example/mylibrary/BookActivity.java
@@ -3,12 +3,36 @@ package com.example.mylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BookActivity extends AppCompatActivity {
+
+    private TextView txtBookName, txtAuthor, txtPages, txtDescription;
+    private Button btnAddToCurrentlyReading, btnAddToWantToRead, btnAddToAlreadyRead, btnAddToFavourite;
+    private ImageView bookImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        initViews();
     }
+
+    private void initViews(){
+        txtBookName = findViewById(R.id.txtBookName);
+        txtAuthor = findViewById(R.id.txtAuthor);
+        txtPages = findViewById(R.id.txtPages);
+        txtDescription = findViewById(R.id.txtDescription);
+
+        btnAddToCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnAddToWantToRead = findViewById(R.id.btnAddToWantToRead);
+        btnAddToAlreadyRead = findViewById(R.id.btnAddToAlreadyRead);
+        btnAddToFavourite = findViewById(R.id.btnFavourite);
+
+        bookImage = findViewById(R.id.bookImage);
+    }
+
 }
